c324040cc6ae284db2fbe7b6b91207a4293c4d80
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2024-01-13 03:25:36 +0100

    add devtools module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
+import { SlotMachineModule } from './slot-machine/slot-machine.module';
 
 @Module({
   imports: [
     UsersModule,
     ConfigModule.forRoot({
-      envFilePath: '../.env',
       isGlobal: true,
     }),
+    DevtoolsModule.register({
+      http: true,
+    }),
+    SlotMachineModule,
   ],
   controllers: [AppController],
   providers: [AppService],
