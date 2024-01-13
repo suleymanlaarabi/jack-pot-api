0ab1ca2c0899457209bc7b69629f3c4a102ae7ba
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2024-01-13 03:28:07 +0100

    new service for play slotMachine

---
+++ src/slot-machine/slot-machine.service.ts
@@ -0,0 +1,69 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/global_services/prisma.service';
+import { UserPayload } from 'src/users/users.service';
+import { PlayMachineDto } from './dto/playMachine.dto';
+import { SlotMachineError } from './types/slotMachine.error';
+
+@Injectable()
+export class SlotMachineService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async playMachine(user: UserPayload, data: PlayMachineDto) {
+    const { id } = user;
+    const { amount } = data;
+
+    let newMoney = (
+      await this.prismaService.user.findUnique({
+        where: { id },
+      })
+    ).money;
+
+    if (newMoney >= amount) {
+      this.prismaService.user.update({
+        where: { id },
+        data: {
+          money: newMoney - amount,
+        },
+      });
+      newMoney -= amount;
+    } else {
+      throw new HttpException(
+        SlotMachineError.NOT_ENOUGH_MONEY,
+        HttpStatus.PAYMENT_REQUIRED,
+      );
+    }
+
+    const roll = [
+      this.getNumberBetween1To7(),
+      this.getNumberBetween1To7(),
+      this.getNumberBetween1To7(),
+    ];
+
+    console.log(roll);
+
+    let multiplier = 0;
+
+    if (roll[0] === roll[1] && roll[1] === roll[2]) {
+      multiplier = 2;
+    }
+
+    if (roll[0] === roll[1] || roll[1] === roll[2] || roll[0] === roll[2]) {
+      multiplier = 1;
+    }
+
+    newMoney = newMoney + amount * multiplier;
+
+    await this.prismaService.user.update({
+      where: { id },
+      data: {
+        money: newMoney,
+      },
+    });
+
+    return newMoney;
+  }
+
+  getNumberBetween1To7() {
+    return Math.round(Math.random() * 6 + 1);
+  }
+}
