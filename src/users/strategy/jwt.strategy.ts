51280b0bed2ab4c97d504e1c24568c379e3e2607
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2024-01-13 03:29:01 +0100

    security: implement jwt strategy

---
+++ src/users/strategy/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserPayload } from '../users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: UserPayload): Promise<UserPayload> {
+    return payload;
+  }
+}
