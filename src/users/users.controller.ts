6c1a418499942b99f8f4659868226a122408a468
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2024-01-13 03:29:17 +0100

    security: add some guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,23 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { UsersService } from './users.service';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { UserPayload, UsersService } from './users.service';
 import { CreateUserDto } from './dto/CreateUser.dto';
 import { LoginUserDto } from './dto/LoginUser.dto';
+import { JwtAuthGuard } from './strategy/jwt-auth.guard';
+
+export interface RequestWithUserPayload extends Request {
+  user: UserPayload;
+}
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-  @Get()
-  async getUsers() {
-    return await this.usersService.getUsers();
-  }
 
   @Post('register')
   async createUser(@Body() userData: CreateUserDto) {
@@ -19,5 +27,11 @@ export class UsersController {
   @Post('login')
   async loginUser(@Body() loginData: LoginUserDto) {
     return await this.usersService.login(loginData);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('auth')
+  async authenticate(@Request() request: RequestWithUserPayload) {
+    return request.user;
   }
 }
